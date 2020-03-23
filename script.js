d7f35a4c48e670268e27f76d52a15985fb8b58e8
Phil <pakphil7@gmail.com>
2020-03-22 22:27:00 -0700

    added local storage buttons, but local storage functionality still needs to be worked on

--- script.js
+++ script.js
@@ -5,15 +5,88 @@ var values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
 
 // This for loop is to create a card element for each AM hour of the day
 for (i = 0; i < 12; i++){
+
+    var row = $('<div class="row">')
+
+    //Add a time slot for each AM hour of the day
     var timeDiv = $('<div class="card time-slot">');
+    $(timeDiv).css({
+        'width': "90%",
+        'max-width': "90%",
+    })
     var labelEl = $("<label>");
     var textAreaEl= $("<textarea class='form-control' rows='3'>");
     var labelElTimeAm = $(labelEl).text(timesArr[i] + ":00 " + "am")
     $(labelEl).attr("value", values[i])
-    $("#schedule").append(timeDiv)
-    $(timeDiv).append(labelEl)
     $(labelEl).append(textAreaEl)
+    $(timeDiv).append(labelEl)
+    $(row).append(timeDiv)
     var checkValues = $(labelEl).attr("value");
+
+    //Add a button element for each time slot (AM)
+    var button = $('<button type="button" class="btn btn-info">Save</button>')
+    $(button).css({
+        'width': "10%",
+        'max-width': "10%",
+    })  
+    $(row).append(button)
+
+    //Display the schedule with the button
+    $("#schedule").append(row);
+
+    //Changes the color of the time slot depending on the current time
+    function colorChange (event){
+        if (checkValues < currentHour) {
+            var pastTimeColor = $(this.timeDiv).css("background-color", "#bababa") && $(this.textAreaEl).css("background-color", "#bababa")
+        }
+        else if (checkValues == currentHour) {
+            var currentTimeColor = $(this.timeDiv).css("background-color", "#99ff99") && $(this.textAreaEl).css("background-color", "#99ff99")
+        }
+        else {
+            var futureTimeColor = $(this.timeDiv).css("background-color", "#add8e6") && $(this.textAreaEl).css("background-color", "#add8e6")
+        }
+    }
+    colorChange()
+
+    $(this.button).click(function() {
+        var textAreaVal = $("textarea").val();
+        var key = $(this.labelEl).attr("value")
+        var value = (this.textAreaVal);
+        localStorage.setItem(key , value);
+    })
+
+}
+
+// This for loop is to create a card element for each PM hour of the day
+for (i = 0; i < 12; i++){
+
+    var row = $('<div class="row">')
+
+    //Add a time slot for each AM hour of the day
+    var timeDiv = $('<div class="card time-slot">');
+    $(timeDiv).css({
+        'width': "90%",
+        'max-width': "90%",
+    })
+    var labelEl = $("<label>");
+    var textAreaEl= $("<textarea class='form-control' rows='3'>");
+    var labelElTimePm = $(labelEl).text(timesArr[i] + ":00 " + "pm")
+    $(labelEl).attr("value", values[i+12])
+    $(labelEl).append(textAreaEl)
+    $(timeDiv).append(labelEl)
+    $(row).append(timeDiv)
+    var checkValues = $(labelEl).attr("value");
+
+    //Add a button element for each time slot (AM)
+    var button = $('<button type="button" class="btn btn-info">Save</button>')
+    $(button).css({
+        'width': "10%",
+        'max-width': "10%",
+    })  
+    $(row).append(button)
+
+    //Display the schedule with the button
+    $("#schedule").append(row);
 
     //Changes the color of the time slot depending on the current time
     function colorChange (event){
@@ -29,31 +102,4 @@ for (i = 0; i < 12; i++){
     }
     colorChange()
 }
-
-
-// This for loop is to create a card element for each PM hour of the day
-for (i = 0; i < 12; i++){
-    var timeDiv = $('<div class="card time-slot">');
-    var labelEl = $("<label>");
-    var textAreaEl= $("<textarea class='form-control' rows='3'>");
-    var labelElTimePm = $(labelEl).text(timesArr[i] + ":00 "+ "pm")
-    $(labelEl).attr("value", values[i+12])
-    $("#schedule").append(timeDiv)
-    $(timeDiv).append(labelEl)
-    $(labelEl).append(textAreaEl)
-    var checkValues = $(labelEl).attr("value");
-    function colorChange (event){
-        if (checkValues < currentHour) {
-            var pastTimeColor = $(this.timeDiv).css("background-color", "#bababa") && $(this.textAreaEl).css("background-color", "#bababa")
-        }
-        else if (checkValues == currentHour) {
-            var currentTimeColor = $(this.timeDiv).css("background-color", "#99ff99") && $(this.textAreaEl).css("background-color", "#99ff99")
-        }
-        else {
-            var futureTimeColor = $(this.timeDiv).css("background-color", "#add8e6") && $(this.textAreaEl).css("background-color", "#add8e6")
-        }
-    }
-    colorChange()
-}
-
 
